b6868e93627c6441f38cf20cbe97063c0420b674
Björn Kautler <Bjoern@Kautler.net>
2024-08-05 04:40:56 +0200

    Add GitHub mirroring

---
+++ .github/workflows/mirror-canonical-repository.main.kts
@@ -0,0 +1,41 @@
+#!/usr/bin/env kotlin
+
+@file:Repository("https://repo.maven.apache.org/maven2/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:2.3.0")
+
+import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
+import io.github.typesafegithub.workflows.domain.triggers.Cron
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.domain.triggers.RepositoryDispatch
+import io.github.typesafegithub.workflows.domain.triggers.Schedule
+import io.github.typesafegithub.workflows.domain.triggers.WorkflowDispatch
+import io.github.typesafegithub.workflows.dsl.expressions.expr
+import io.github.typesafegithub.workflows.dsl.workflow
+import io.github.typesafegithub.workflows.yaml.ConsistencyCheckJobConfig.Disabled
+
+workflow(
+    name = "Mirror Canonical Repository",
+    on = listOf(
+        Push(),
+        RepositoryDispatch(),
+        Schedule(triggers = listOf(Cron(minute = "44"))),
+        WorkflowDispatch()
+    ),
+    sourceFile = __FILE__,
+    consistencyCheckJobConfig = Disabled
+) {
+    job(
+        id = "mirror_repository",
+        name = "Mirror Repository",
+        runsOn = UbuntuLatest
+    ) {
+        run(
+            name = "Clone Canonical Repository",
+            command = "git clone --bare git://git.code.sf.net/p/jedit/jEdit.bak ."
+        )
+        run(
+            name = "Push To Mirror Repository",
+            command = "git push --mirror https://x:${expr { github.token }}@github.com/${expr { github.repository }}"
+        )
+    }
+}
