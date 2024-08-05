53cb0d7a284e1b2ffa310e599dbc91c1e6c57255
Björn Kautler <Bjoern@Kautler.net>
2024-08-05 04:40:56 +0200

    Add GitHub mirroring

--- .github/workflows/mirror-canonical-repository.main.kts
+++ .github/workflows/mirror-canonical-repository.main.kts
@@ -9,6 +9,7 @@ import io.github.typesafegithub.workflows.domain.triggers.Push
 import io.github.typesafegithub.workflows.domain.triggers.RepositoryDispatch
 import io.github.typesafegithub.workflows.domain.triggers.Schedule
 import io.github.typesafegithub.workflows.domain.triggers.WorkflowDispatch
+import io.github.typesafegithub.workflows.dsl.expressions.Contexts.secrets
 import io.github.typesafegithub.workflows.dsl.expressions.expr
 import io.github.typesafegithub.workflows.dsl.workflow
 import io.github.typesafegithub.workflows.yaml.ConsistencyCheckJobConfig.Disabled
@@ -18,7 +19,7 @@ workflow(
     on = listOf(
         Push(),
         RepositoryDispatch(),
-        Schedule(triggers = listOf(Cron(minute = "44"))),
+//        Schedule(triggers = listOf(Cron(minute = "44"))),
         WorkflowDispatch()
     ),
     sourceFile = __FILE__,
@@ -33,9 +34,10 @@ workflow(
             name = "Clone Canonical Repository",
             command = "git clone --bare git://git.code.sf.net/p/jedit/jEdit.bak ."
         )
+        val MIRROR_TOKEN by secrets
         run(
             name = "Push To Mirror Repository",
-            command = "git push --mirror https://x:${expr { github.token }}@github.com/${expr { github.repository }}"
+            command = "git push --mirror https://x:${expr(MIRROR_TOKEN)}@github.com/${expr { github.repository }}"
         )
     }
 }
